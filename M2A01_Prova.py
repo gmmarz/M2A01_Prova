91913299107f350dd27eafe7c1782f8b89640c50
Guilherme Marzano <guilhermemarzano@gmail.com>
2023-10-20 23:03:58 -0300

    'Enunciado'

--- M2A01_Prova.py
+++ M2A01_Prova.py
@@ -1,2 +1,3 @@
 #Aluno: Guilherme Melo Marzano
-#Enunciado - 
+#Enunciado - Programa que cadastra um email e uma senha e em seguida pede para o usuário digitar o email e a senha.
+#          - Enquanto o email e senha for inválido continua no loop assim que acertar finaliza
